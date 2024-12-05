db503f64b532f82343f7dcece750afc59051ce9d
avoiddeveloper <karimwdd03@gmail.com>
2024-12-06 03:39:47 +0600

    JS Work Done

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,75 @@
-console.log('hello')
+// Tab Toggle
+
+// Donate
+document.getElementById('donationBtn').addEventListener('click', function (event) {
+    document.getElementById('historyTab').classList.add('hidden');
+    document.getElementById('donationTab').classList.remove('hidden');
+    document.getElementById('historyBtn').classList.remove('bg-themePrimary')
+    document.getElementById('donationBtn').classList.add('bg-themePrimary')
+
+})
+
+// History
+document.getElementById('historyBtn').addEventListener('click', function (event) {
+    document.getElementById('donationBtn').classList.remove('bg-themePrimary')
+    document.getElementById('donationTab').classList.add('hidden');
+    document.getElementById('historyTab').classList.remove('hidden');
+    event.target.classList.add('bg-themePrimary');
+
+})
+
+
+// Card - 1
+function donate1() {
+    const donateAmount = getInputValue('donate1');
+    if (donateAmount < 0) {
+        return alert('Please insert Positive Amount')
+    } else {
+        const currentBalance = parseFloat(document.getElementById('balance').innerText);
+        if (currentBalance !== 0) {
+            mainBalance(donateAmount);
+            donatedAmount('donatedOneBalance', donateAmount);
+            document.getElementById('my_modal_5').showModal();
+            createHistory('title1', donateAmount, 'historyTab');
+
+        } else {
+            return alert('insufficient balance')
+        }
+    }
+}
+
+// Card - 2
+function donate2() {
+    const donateAmount = getInputValue('donate2');
+    if (donateAmount < 0) {
+        return alert('Please insert Positive Amount')
+    } else {
+        const currentBalance = parseFloat(document.getElementById('balance').innerText);
+        if (currentBalance !== 0) {
+            mainBalance(donateAmount)
+            donatedAmount('donatedTwoBalance', donateAmount);
+            document.getElementById('my_modal_6').showModal();
+            createHistory('title2', donateAmount, 'historyTab');
+        } else {
+            return alert('insufficient balance')
+        }
+    }
+}
+
+// Card - 2
+function donate3() {
+    const donateAmount = getInputValue('donate3');
+    if (donateAmount < 0) {
+        return alert('Please insert Positive Amount')
+    } else {
+        const currentBalance = parseFloat(document.getElementById('balance').innerText);
+        if (currentBalance !== 0) {
+            mainBalance(donateAmount)
+            donatedAmount('donatedThreeBalance', donateAmount);
+            document.getElementById('my_modal_7').showModal();
+            createHistory('title3', donateAmount, 'historyTab');
+        } else {
+            return alert('insufficient balance')
+        }
+    }
+}
---
+++ JS/utilities.js
@@ -0,0 +1,28 @@
+// Get Input Value
+function getInputValue(id) {
+    return parseFloat(document.getElementById(id).value);
+}
+
+//Get And Set Main Balance
+function mainBalance(i) {
+    const currentBalance = parseFloat(document.getElementById('balance').innerText); //Main Balance
+    document.getElementById('balance').innerText = currentBalance - i
+}
+
+// Get and set donated amount
+function donatedAmount(id, value) {
+    const currentAmount = parseFloat(document.getElementById(id).innerText);
+    document.getElementById(id).innerText = currentAmount + value;
+}
+
+// Add to History 
+function createHistory(title, amount, mainDiv) {
+    const topic = document.getElementById(title).innerText;
+    const div = document.createElement('div');
+    div.classList.add('border', 'rounded-2xl', 'p-8', 'space-y-2')
+    div.innerHTML = `
+        <p class="font-bold text-xl text-mainText">${amount} Taka is ${topic}</p>
+        <p class="font-light text-base text-subText">Time : ${new Date().toLocaleTimeString()}  Date : ${new Date().toLocaleDateString()}</p>
+    `
+    document.getElementById(mainDiv).appendChild(div)
+}
